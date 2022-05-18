a8117af41d19f1c1d5e6310aeaf55e0609105fb3
Jarod Santiago Viquez Salazar <jarod281@hotmail.com>
2022-05-17 21:01:16 -0600

    Creacion del proyecto

---
+++ src/main/java/com/TiendaJarod/TiendaJarodApplication.java
@@ -0,0 +1,13 @@
+package com.TiendaJarod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaJarodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaJarodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TiendaJarod/TiendaJarodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TiendaJarod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaJarodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
